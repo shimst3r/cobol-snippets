8bcaf05a9ae2e7ebe4706003d324de9d94062bad
Nils <shimst3r@gmail.com>
2024-11-03 15:18:12 +0100

    fix: Add missing periods in hello_world.cob

--- src/hello_world.cob
+++ src/hello_world.cob
@@ -1,8 +1,8 @@
        IDENTIFICATION DIVISION.
        PROGRAM-ID. HelloWorld.
       *Program that prints "Hello, World!" to the screen.
-       AUTHOR Nils Müller.
-       DATE-WRITTEN 2024-11-03.
+       AUTHOR. Nils Müller.
+       DATE-WRITTEN. 2024-11-03.
        PROCEDURE DIVISION.
        DisplayPrompt.
            DISPLAY "Hello, World!".
